8ab96cf03fff962df059a4540306f2c1124a7bf7
Romain SOMMERARD <romain.sommerard@gmail.com>
2014-10-22 13:39:39 +0200

    PersonalTokens added.

---
+++ PJETwitter/src/pjetwitter/PersonalTokens.java
@@ -0,0 +1,8 @@
+package pjetwitter;
+
+public class PersonalTokens {
+	public static String CONSUMER_KEY = "";
+	public static String CONSUMER_SECRET = "";
+	public static String ACCESS_TOKEN = "";
+	public static String ACCESS_TOKEN_SECRET = "";
+}
